cb5a547c4ad83db199f4f568d84e6afdd12702b4
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-06 20:54:03 +0300

    форма 2 способ useActionData()

--- src/pages/Editpost.jsx
+++ src/pages/Editpost.jsx
@@ -1,14 +1,53 @@
-import {useParams} from "react-router-dom";
+import {
+    useActionData,
+    useLoaderData,
+    useNavigation, /*useParams*/
+} from "react-router-dom";
+import UpdatePost from "../components/UpdatePost";
 
-// редактировать пост
 const Editpost = () => {
-    const {id} = useParams();
+    // const {id} = useParams();
+
+    // вернет 1 объект с ключом message стр. 43
+    // далее отрисовываем строка 16 выведет data.message
+    const data = useActionData();
+    const {post, id} = useLoaderData();
+    // статус отправки будем узнавать
+    const navigation = useNavigation();
+
     return (
         <div>
-            {/*уточняем какой именно посто хотим редактировать*/}
+            {data?.message && <div style={{color: "blue"}}>{data.message}</div>}
             <h1>Edit post {id}</h1>
+            <UpdatePost {...post} submitting={navigation.state === "submitting"}/>
         </div>
     );
 };
+// асинхр функция для отправки на сервак данных
+// post это formData
+const updatePost = async (post) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.get("id")}`, {
+        method: "PUT",
+        // передаем post не обязательно как в Createpost использовать  JSON.stringify() - и так работать будет
+        // так как передаем formData при таком подходе не передаем: headers: {"Content-Type": "application/json"},
+        body: post
+    });
+    return res.json();
+};
+// ее надо отправить в router.js стр 27
+// request - объект будет знать о форме
+const updatePostAction = async ({request}) => {
+    const formData = await request.formData();
 
-export {Editpost};
+    // валидация:
+    // есть ли title у formData и body?
+    if (!formData.get("title") || !formData.get("body")) {
+        return {message: "All field are required!!!"};
+    }
+
+    const updatedPost = await updatePost(formData);
+// обработай ответ (чтобы получить это сообщение..) на уровне приложения используй useActionData() см вверх...
+    return {message: `Post ${updatedPost.id} was successfully updated`};
+};
+
+export {Editpost, updatePostAction};
