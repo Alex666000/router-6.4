5b0235cce712a323b9c61ce523f47034fbf7b027
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-06 19:28:57 +0300

    loader --- без defer в Postpage

--- src/pages/Postpage.jsx
+++ src/pages/Postpage.jsx
@@ -6,87 +6,92 @@ import {
     useLoaderData,
     useNavigate,
     useAsyncValue,
-    useParams
+    useParams, Await
 } from "react-router-dom";
 
 const Post = () => {
-    // чтобы не писать resolvedPosts как в Blogpage
-    const post = useAsyncValue();
+    const post = useAsyncValue()
 
     return (
         <>
-            <h1>{post && post.title}</h1>
-            <p>{post && post.body}</p>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
         </>
-    );
-};
+    )
+}
 
-export const Postpage = () => {
-    // const {id} = useParams();
-    const {post, id} = useLoaderData();
-
-    // для перемещения по истории переключения страниц
-    const navigate = useNavigate();
-
-    // const [post, setPost] = useState<ResponseData | null>(null);
-
-// для этого в разметке делаем кнопку
-    const goBack = () => navigate(-1);
-
-    /* useEffect(() => {
-         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-             .then(res => res.json())
-             .then(data => setPost(data));
-     }, [id]);*/
-
+const Comments = () => {
+    // тут достанем комменты теперь
+    const comments = useAsyncValue()
 
     return (
         <div>
-            {/*для перемещения по истории делаем кнопку*/}
-            <button onClick={goBack}>Go back</button>
-            {/*<h1>{post && post.title}</h1>*/}
-            {/*<p>{post && post.body}</p>*/}
-            {/*ссылка будет вести на пост который пришел в параметре - id*/}
-            <Suspense>
-                <Await resolve={post} fallback={<h2>Loading...</h2>}>
-                    {/*когда пост получим отрисуем*/}
-                    <Post/>
-                </Await>
-            </Suspense>
-            <Link to={`/post${id}`}>Редактировать пост</Link>
+            <h2>Comments</h2>
+            {comments.map(comment => (
+                <>
+                    <h3>{comment.email}</h3>
+                    <h4>{comment.name}</h4>
+                    <p>{comment.body}</p>
+                </>
+            ))}
         </div>
-    );
-};
-
-async function getPostId(id) {
-    const res = fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    // post получили один...по id...
-    const post = await res.json();
-    return {post, id};
+    )
 }
 
-export const postLoader = async ({params}) => {
-    /*id так называется имя в роутинге <Route path="posts/:id" element={<Postpage/>}/>*/
-    const id = params.id;
-    // id получает сразу а пост через вызов...
-    return {post: getPostId(id), id};
+export const Postpage = () => {
+    const { post, id, comments } = useLoaderData()
+    const navigate = useNavigate();
 
+    const goBack = () => navigate(-1);
 
+    return (
+        <div>
+            <button onClick={goBack}>Go back</button>
+            <Suspense fallback={<h2>Post is loading...</h2>}>
+                <Await resolve={post}>
+                    <Post />
+                </Await>
+            </Suspense>
+            <Suspense fallback={<h2>Comments is loading...</h2>}>
+                <Await resolve={comments}>
+                    <Comments />
+                </Await>
+            </Suspense>
+            <Link to={`/posts/${id}/edit`}>Edit this post</Link>
+        </div>
+    )
 };
 
+async function getPostById(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    return res.json()
+}
+async function getCommentsByPost(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+    return res.json()
+}
+// без defer
+const postLoader = async ({ params }) => {
+    const id = params.id;
+// нужны сразу пост по id поэтому пишем await а комменты потом внутри загрузим...
+    return defer({ post: await getPostById(id), id, comments: getCommentsByPost(id) })
+}
+
 /*
-при работе со страницей понадобится params - получаем id через params - удалим useParams, useState
+- при работе со страницей понадобится params - получаем id через params - удалим useParams, useState
 остальное как в Blogpage
 
-не забываем лоадер использовать в роутинге
+- не забываем лоадер использовать в роутинге
  <Route path="posts/:id" element={<Postpage/>} loader={postLoader}/>
 
- минусы такого подхода - ждем пока страница подгрузится....библа предоставляет хелпер defer
+ - минусы такого подхода - ждем пока страница подгрузится....библа предоставляет хелпер defer
 
-вынесли в компонент отдельный отрисовку и в ней тогда обязательно раз вынесли...
+- вынесли в компонент отдельный отрисовку и в ней тогда обязательно раз вынесли...
  если б не выносили как тут то не надо было бы делать useAsyncValue
 
- defer - разделяет сущности -- он в принципе не нужен можно без него - мы сделали с ним для разделения сущностей
+ - defer - разделяет сущности -- он в принципе не нужен можно без него - мы сделали с ним для разделения сущностей
+
+ - но лучше с дефером делать!!! Чтобы прелоадер был при ожидании!!!
 
  */
 
